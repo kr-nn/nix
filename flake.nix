a20388b2b1f5e2d79bf3d878e016a943480422f4
kyle <kyle@nocturnalnerd.xyz>
2024-08-24 19:36:08 -0400

    I can now use both stable an unstable packages when I need

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
   inputs = {
 
     # NIXPKGS
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     # HARDWARE
     nixos-hardware.url = "github:nixos/nixos-hardware";
     # Stylix
@@ -12,17 +13,19 @@
 
   };
 
-  outputs = { nixpkgs, stylix, nixos-hardware, ... }:
+  outputs = { nixpkgs-stable, nixpkgs-unstable, stylix, nixos-hardware, ... }:
 
   # ARGS ========================================================================
     let
       system = "x86_64-linux";
+      pkgs-unstable = import nixpkgs-unstable {inherit system; config.allowUnfree = true; };
     in {
   # NIXOS ========================================================================
 
     # Framework laptop
-    nixosConfigurations."sorin" = nixpkgs.lib.nixosSystem {
+    nixosConfigurations."sorin" = nixpkgs-stable.lib.nixosSystem {
      inherit system;
+     extraArgs = { inherit pkgs-unstable; };
      modules = [
        ./hosts/sorin/configuration.nix
        nixos-hardware.nixosModules.framework-13-7040-amd
