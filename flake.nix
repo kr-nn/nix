ed2bda95bae14037beea2b3772c9ff946246ff3c
kyle <kyle@nocturnalnerd.xyz>
2024-08-26 13:05:09 -0400

    added bleeding edge pkgs

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
 
     # NIXPKGS
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-bleeding.url = "github:nixos/nixpkgs/master";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
     # HOME-MANAGER
@@ -27,12 +28,13 @@
 
   };
 
-  outputs = { nixpkgs-unstable, nixpkgs-stable, agenix, stylix, home-manager, ... }:
+  outputs = { nixpkgs-unstable, nixpkgs-bleeding, nixpkgs-stable, agenix, stylix, home-manager, ... }:
 
   # ARGS ========================================================================
     let
       system = "x86_64-linux";
       pkgs-unstable = import nixpkgs-unstable {inherit system; config.allowUnfree = true; };
+      pkgs-bleeding = import nixpkgs-bleeding {inherit system; config.allowUnfree = true; };
       pkgs-stable = import nixpkgs-stable {inherit system; config.allowUnfree = true; };
     in {
 
@@ -40,7 +42,7 @@
 
     homeConfigurations."kyle" = home-manager.lib.homeManagerConfiguration {
       pkgs = pkgs-unstable;
-      extraSpecialArgs = { inherit pkgs-stable; };
+      extraSpecialArgs = { inherit pkgs-stable; inherit pkgs-bleeding; };
       modules = [
         ./hm/kyle.nix
         stylix.homeManagerModules.stylix
