839f1ad7468d6e40c13e7cd38a51e837e5e86368
kyle <kyle@nocturnalnerd.xyz>
2024-12-21 01:28:58 -0500

    declarative displaylink

--- hosts/_mods/displaylink.nix
+++ hosts/_mods/displaylink.nix
@@ -1,11 +1,18 @@
 { pkgs, ... }:
 {
-  #displaylink = pkgs.fetchurl {
-  #  url = "https://www.synaptics.com/sites/default/files/exe_files/2023-08/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu5.8-EXE.zip";
-  #  sha256 = "05m8vm6i9pc9pmvar021lw3ls60inlmq92nling0vj28skm55i92";
-  #};
+  nixpkgs.overlays = [
+    (final: prev: {
+      displaylink = prev.displaylink.overrideAttrs {
+        src = ../../assets/displaylink-580.zip;
+      };
+    })
+  ];
 
   services.xserver = {
     videoDrivers = [ "displaylink" "modesetting" ];
   };
+
+  environment.systemPackages = [
+    pkgs.displaylink
+  ];
 }
