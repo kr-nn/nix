a6d32d33008ef76c683293e7e66155e17334e32e
kyle <kyle@nocturnalnerd.xyz>
2024-12-23 12:30:19 -0500

    removed vivaldi for home-manager use

--- hosts/sorin/configuration.nix
+++ hosts/sorin/configuration.nix
@@ -18,7 +18,7 @@ let
   # OS things ==================================================
   main = lib.mkMerge [ theme {
 
-    environment.systemPackages = with pkgs; [ vivaldi vivaldi-ffmpeg-codecs widevine-cdm lockscreenWallpaper fwupd framework-tool ];
+    environment.systemPackages = with pkgs; [ lockscreenWallpaper fwupd framework-tool ];
 
     ## Bootloader ==============================================================
     boot.kernelPackages = pkgs.linuxPackages;
