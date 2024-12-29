320306f88d1175a180ce9018e81aab5664d780d6
kyle <kyle@nocturnalnerd.xyz>
2024-12-29 12:57:26 -0500

    updated home-manager alias to auto backup files

--- hm/_home.nix
+++ hm/_home.nix
@@ -270,8 +270,8 @@ zshDefault = {
       hmcd="cd ~/.config/home-manager/";
       hmll="ll ~/.config/home-manager/";
       hmrc="hmcd && $EDITOR ~/.config/home-manager/hm/_home.nix && cd -";
-      hmsw="home-manager switch";
-      hmbu="home-manager build";
+      hmsw="home-manager switch -b ~/.hmbak";
+      hmbu="home-manager build -b ~/.hmbak";
 
       # convenience
       fdbench = "time fd --type d . / --max-depth 1 | parallel time fd . {}|grep ^fd"; # Benchmarks the high performance search of fzf
