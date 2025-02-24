e2743bb75f08c6d26d6dd2d4c87dcbf09457c5b9
kyle <kyle@nocturnalnerd.xyz>
2025-02-23 23:01:52 -0500

    removed syncthing reproducible folders

--- hosts/_mods/syncthing.nix
+++ hosts/_mods/syncthing.nix
@@ -14,12 +14,6 @@
         urAccepted = -1;
         relaysEnabled = false;
       };
-      devices = {
-        "vault".id = "SFVOF3W-MSEE5ME-XWB4Q7Q-EZMGMGG-7LVD6FW-LLINHCE-IJNQFU7-BRLJUAQ";
-      };
-      folders = {
-        "/home/kyle/Knowledge".devices = ["vault"];
-      };
     };
   };
 
