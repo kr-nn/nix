60ce90aa3402e2737908c94ed8e1e9da5c8a821a
krobinson <kyle.robinson@nocturnalnerd.xyz>
2025-05-15 12:50:10 -0400

    ensure nix command shortcuts eval against master branch

--- hm/_home.nix
+++ hm/_home.nix
@@ -198,11 +198,11 @@ packagesDefault = { home.packages = with pkgs; [
   (pkgs.writeShellScriptBin "nobo"  ''nixos-rebuild boot'')
   (pkgs.writeShellScriptBin "nobu"  ''nixos-rebuild build'')
 
-  (pkgs.writeShellScriptBin "ns"    ''nix search github:nixos/nixpkgs '')
-  (pkgs.writeShellScriptBin "nr"    ''nix run github:nixos/nixpkgs#"$1" -- ''${@:2}'')
-  (pkgs.writeShellScriptBin "nsh"    ''nix shell github:nixos/nixpkgs#"$1" -- ''${@:2}'')
-  (pkgs.writeShellScriptBin "nri"   ''nix run github:nixos/nixpkgs#"$1" --impure -- ''${@:2}'')
-  (pkgs.writeShellScriptBin "nshi"   ''nix shell github:nixos/nixpkgs#"$1" --impure -- ''${@:2}'') ];};
+  (pkgs.writeShellScriptBin "ns"    ''nix search github:nixos/nixpkgs/master '')
+  (pkgs.writeShellScriptBin "nr"    ''nix run github:nixos/nixpkgs/master#"$1" -- ''${@:2}'')
+  (pkgs.writeShellScriptBin "nsh"    ''nix shell github:nixos/nixpkgs/master#"$1" -- ''${@:2}'')
+  (pkgs.writeShellScriptBin "nri"   ''nix run github:nixos/nixpkgs/master#"$1" --impure -- ''${@:2}'')
+  (pkgs.writeShellScriptBin "nshi"   ''nix shell github:nixos/nixpkgs/master#"$1" --impure -- ''${@:2}'') ];};
 
 envDefault = {
   home.sessionVariables = {
