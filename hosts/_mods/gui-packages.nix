87e5aaa8ee06db99f43d266d1994f50dba043f30
kyle <kyle@nocturnalnerd.xyz>
2025-10-24 18:37:30 -0400

    moved yakuake to nixos config

--- hosts/_mods/gui-packages.nix
+++ hosts/_mods/gui-packages.nix
@@ -4,5 +4,6 @@
     pkgs.vivaldi # vivaldi requires matching QT library of display manager, do not place in home-manager
     pkgs.vivaldi-ffmpeg-codecs
     pkgs.widevine-cdm
+    pkgs.kdePackages.yakuake # yakuake requires compatible QT version
   ];
 }
