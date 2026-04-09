c3d41cb5bf744dd9457aa8dfb3e1b1ed11e51455
kyle <kyle@nocturnalnerd.xyz>
2026-04-08 21:32:06 -0400

    fixed zerotier being disabled entirely

--- hosts/_mods/zerotier.nix
+++ hosts/_mods/zerotier.nix
@@ -5,8 +5,9 @@
     insertNameservers = [ "10.0.0.1" ];
   };
 
+  systemd.services.zerotierone.enable = false;
   services.zerotierone = {
-    enable = false;
+    enable = true;
     joinNetworks = [ "565799d8f6032b14" ]; # This network can only manually join devices to the network
   };
 
