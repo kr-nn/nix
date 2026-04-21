2cccff8cff0b1d98ae6a5fab9037ab659cbbcf98
kyle <kyle@nocturnalnerd.xyz>
2026-04-20 23:01:21 -0400

    systemd shenanigans

--- hosts/_mods/zerotier.nix
+++ hosts/_mods/zerotier.nix
@@ -5,7 +5,7 @@
     insertNameservers = [ "10.0.0.1" ];
   };
 
-  systemd.services.zerotierone.enable = false;
+  systemd.services.zerotierone.wantedBy = [];
   services.zerotierone = {
     enable = true;
     joinNetworks = [ "565799d8f6032b14" ]; # This network can only manually join devices to the network
