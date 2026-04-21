e1411d06c11e98556759ec529f822f1a122f0429
kyle <kyle@nocturnalnerd.xyz>
2026-04-20 23:20:48 -0400

    maybe this works?

--- hosts/_mods/zerotier.nix
+++ hosts/_mods/zerotier.nix
@@ -1,11 +1,11 @@
-{ ... }:
+{ lib, ... }:
 {
 
   networking.networkmanager = {
     insertNameservers = [ "10.0.0.1" ];
   };
 
-  systemd.services.zerotierone.wantedBy = [];
+  systemd.services.zerotierone.wantedBy = lib.mkForce [];
   services.zerotierone = {
     enable = true;
     joinNetworks = [ "565799d8f6032b14" ]; # This network can only manually join devices to the network
