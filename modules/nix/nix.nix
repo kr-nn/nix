718674099fb229a26c1d6e159f2f60bf740e0218
kyle <kyle@nocturnalnerd.xyz>
2026-05-07 16:14:14 -0400

    added nix cache for hydra and proxmox-nixos

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -9,5 +9,7 @@ in
       home-manager = inputs.home-manager;
       flake-parts = inputs.flake-parts;
     };
+    nix.settings.trusted-substituters = [ "https://hydra.nixos.org/" "https://cache.saumon.network/proxmox-nixos" ];
+    nix.settings.trusted-public-keys = [ "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs=" "cache.saumon.network:proxmox-nixos:D9RYSWpQQC/msZUWphOY2I5RLH5Dd6yQcaHIuug7dWM=" ];
   };
 }
