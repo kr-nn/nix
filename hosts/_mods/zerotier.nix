3c98844e984cf7466764c303b3dcc3c7a20be498
kyle <kyle@nocturnalnerd.xyz>
2026-06-30 18:00:01 -0400

    zerotier update

--- hosts/_mods/zerotier.nix
+++ hosts/_mods/zerotier.nix
@@ -8,7 +8,10 @@
   systemd.services.zerotierone.wantedBy = lib.mkForce [];
   services.zerotierone = {
     enable = true;
-    joinNetworks = [ "565799d8f6032b14" ]; # This network can only manually join devices to the network
+    joinNetworks = [
+      "1d71939404bf62a5" # work
+      "565799d8f6032b14" # personal
+    ]; # This network can only manually join devices to the network
   };
 
 }
