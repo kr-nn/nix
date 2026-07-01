24e12a78a54c5e7b77046774ab2009ad0d759f43
kyle <kyle@nocturnalnerd.xyz>
2026-06-30 20:34:19 -0400

    removed personal zerotier

--- hosts/_mods/zerotier.nix
+++ hosts/_mods/zerotier.nix
@@ -10,7 +10,6 @@
     enable = true;
     joinNetworks = [
       "1d71939404bf62a5" # work
-      "565799d8f6032b14" # personal
     ]; # This network can only manually join devices to the network
   };
 
