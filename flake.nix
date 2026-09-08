4aed56aa14288d1093bdbc721f4467d7686a527c
kyle <kyle@nocturnalnerd.xyz>
2026-09-08 10:56:28 -0400

    aligned inputs

--- flake.nix
+++ flake.nix
@@ -3,17 +3,17 @@
 
   inputs = {
 
-    ## Required
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
-    nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
+    ## lib
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixpkgs"; };
+    import-tree = { url = "github:vic/import-tree"; }; # import tree has no inputs
 
     ## packages
     multiverse.url = "github:fzakaria/nixpkgs-multiverse";
-    neix.url = "github:Hovirix/neix";
+    neix = { url = "github:Hovirix/neix"; inputs.nixpkgs.follows = "multiverse"; };
+    nix-index = { url = "github:nix-community/nix-index-database"; inputs.nixpkgs.follows = "multiverse"; };
 
     ## Modules
-    nix-index = { url = "github:nix-community/nix-index-database"; inputs.nixpkgs.follows = "nixpkgs"; };
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
     agenix = { url = "github:ryantm/agenix"; inputs.nixpkgs.follows = "nixpkgs"; };
     stylix = { url = "github:nix-community/stylix"; inputs.nixpkgs.follows = "nixpkgs"; };
@@ -27,8 +27,9 @@
       };
     }
     (inputs.import-tree [
-      inputs.home-manager.flakeModules.home-manager               # Import home-manager flake-parts module
-      #(inputs.import-tree ./modules)
-      (inputs.import-tree.matchNot ''.*/secrets\.nix'' ./modules) # Import everything except secrets.nix
+      inputs.home-manager.flakeModules.home-manager
+        (inputs.import-tree.matchNot
+          ''.*/secrets\.nix''
+        ./modules )
     ]);
 }
